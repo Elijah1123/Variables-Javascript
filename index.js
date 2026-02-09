b5447e9fa49f42dd36870500543b81b4358770ea
elijah1123 <elijahmzalendo659@gmail.com>
2026-02-09 18:47:20 +0300

    variables

---
+++ index.js
@@ -0,0 +1,27 @@
+// Variables = A container that store a value.
+//             Behaves as if it were the value it contains
+
+
+// 1. declaration let x;
+// 2. assignment x = 100;
+
+
+// let age = 25;
+// let price = 500;
+// let distance = 4
+
+
+
+// console.log(`The price is Ksh ${price}`);
+// console.log(`You are ${age} years old`);
+// console.log(`The distance from home to town is  ${distance} Km`);
+
+let firstName = "Elijah"
+let secondName = "Mzalendo"
+let favoriteFood = "Pizza";
+let email = "elijahmzalendo@gmail.com"
+ 
+console.log(typeof (firstName))
+console.log(`My name is ${firstName} ${secondName}`)
+console.log(`My favorite food is ${favoriteFood}`)
+console.log(`My email is ${email}`) 
