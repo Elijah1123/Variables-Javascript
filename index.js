32fad0735ba45f30b07d74537800dd562c7d944c
elijah1123 <elijahmzalendo659@gmail.com>
2026-02-16 09:59:51 +0300

    variable

--- index.js
+++ index.js
@@ -16,12 +16,22 @@
 // console.log(`You are ${age} years old`);
 // console.log(`The distance from home to town is  ${distance} Km`);
 
-let firstName = "Elijah"
-let secondName = "Mzalendo"
-let favoriteFood = "Pizza";
-let email = "elijahmzalendo@gmail.com"
- 
-console.log(typeof (firstName))
-console.log(`My name is ${firstName} ${secondName}`)
-console.log(`My favorite food is ${favoriteFood}`)
-console.log(`My email is ${email}`) 
+// let firstName = "Elijah"
+// let secondName = "Mzalendo"
+// let favoriteFood = "Pizza";
+// let email = "elijahmzalendo@gmail.com"
+   
+// console.log(typeof (firstName))
+// console.log(`My name is ${firstName} ${secondName}`)
+// console.log(`My favorite food is ${favoriteFood}`)
+// console.log(`My email is ${email }`) 
+
+
+let  fullName = "Elijah Mzalendo";
+let age = 25; 
+let isStudent   = false;
+
+document.getElementById("p1").textContent =  `You name is ${fullName}`;
+document.getElementById("p2").textContent =  `You are ${age} years old`;
+document.getElementById("p3").textContent =  `Enrolled: ${isStudent}`
+  
